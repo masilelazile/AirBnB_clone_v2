df288da50e7a56a5a52503b94137d9148ee353a9
masilelazile <masilelazile@gmail.com>
2023-11-22 08:30:50 -0800

    updated

--- models/user.py
+++ models/user.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """Defines the User class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.base_model import Base
 
-
-class User(BaseModel, Base):
+class User(Base):
     """Represents a user for a MySQL database.
 
     Inherits from SQLAlchemy Base and links to the MySQL table users.
@@ -22,9 +19,19 @@ class User(BaseModel, Base):
         reviews (sqlalchemy relationship): The User-Review relationship.
     """
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
+
+    id = Column(String(36), primary_key=True, nullable=False)
+    email = Column(String(128), nullable=False, unique=True)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+
+    places = relationship("Place", backref="user", cascade="all, delete-orphan")
+    reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
+
+    def __init__(self, email, password, first_name=None, last_name=None):
+        """Initialize a new User."""
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
